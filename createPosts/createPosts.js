258a75a5e1286980fab21b968d5c7660ff6761d6
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-04 12:58:44 -0800

    .

---
+++ createPosts/createPosts.js
@@ -0,0 +1,15 @@
+let txtArea = document.getElementById('txtArea');
+let crtBttn = document.getElementById('crtBttn');
+let dltBttn = document.getElementById('dltBttn');
+
+// crtBttn.addEventListener('click', () => {
+//     console.log(txtArea.value);
+// });
+
+let crtFun = () => {
+    console.log(txtArea.value);
+}
+
+
+
+//ab iskey under mujhe addDoc karwana he 
