f0812512d6e8a209f03447e543f3ab8acda3c47e
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-05 10:24:40 -0800

    .

--- createPosts/createPosts.js
+++ createPosts/createPosts.js
@@ -1,14 +1,32 @@
+import {
+    collection,
+    addDoc,
+    getDocs,
+} from 'https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js'
+
+import { db } from '../firebaseConfig.js';
+
 let txtArea = document.getElementById('txtArea');
 let crtBttn = document.getElementById('crtBttn');
 let dltBttn = document.getElementById('dltBttn');
 
-// crtBttn.addEventListener('click', () => {
-//     console.log(txtArea.value);
-// });
-
-let crtFun = () => {
+crtBttn.addEventListener('click', () => {
     console.log(txtArea.value);
-}
+});
+
+// let crtFun = async() => {
+//     console.log(txtArea.value);
+//     try {
+//         const docRef = await addDoc(collection(db, "Posts"), {
+//           first: "Ada",
+//           last: "Lovelace",
+//           born: 1815
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//       } catch (e) {
+//         console.error("Error adding document: ", e);
+//       }
+// }
 
 
 
