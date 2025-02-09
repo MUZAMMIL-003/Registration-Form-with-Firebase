48c7ce24252839a3672aea5cebd2538af13d0474
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-02-09 11:24:55 -0800

    .

--- myPosts/muPosts.js
+++ myPosts/muPosts.js
@@ -43,41 +43,91 @@ let user = JSON.parse(localStorage.getItem("user"));
 
 const q = query(UserPfp, where("Userid", "==", user));
 
+// const querySnapshot = await getDocs(q);
+// querySnapshot.forEach((doc1) => {
+//   let posts = doc1.data().post;
+//   let Userid = doc1.data().Userid;
+//   let username = doc1.data().userName 
+//   let docID = doc1.id;
+//   console.log(docID)
+// // console.log(posts)
+//   let rowDiv = document.querySelector(".row");
+//   let tempDiv = `
+// <div class="col">
+// <div class="card h-100">
+// <div class="card-body">
+// <h5 class="card-title">${posts}</h5>
+// <p class="card-text">Written By =>${ username}</p>
+// <a href="#" class="card-link" id="Update">Update Post</a>
+// <a href="#" class="card-link" id="delete">Delete Post</a>
+// </div>
+// </div>
+// </div>
+// `
+// rowDiv.innerHTML += tempDiv;
+
+// let deleteBttn = document.getElementById("delete");
+// deleteBttn.addEventListener("click", async () => {
+//   await deleteDoc(doc(db, "Posts", docID));
+//   window.location.reload();
+// })
+
+// let updateBttn = document.getElementById("Update");
+// updateBttn.addEventListener("click", async () => {
+//   updateFunc(docID)
+//   let input = prompt(" enter here",posts)
+//   console.log(input)
+// })
+
+// });
+
 const querySnapshot = await getDocs(q);
 querySnapshot.forEach((doc1) => {
   let posts = doc1.data().post;
   let Userid = doc1.data().Userid;
-  let username = doc1.data().userName 
+  let username = doc1.data().userName;
   let docID = doc1.id;
-  console.log(docID)
-// console.log(posts)
+
+  console.log(docID);  // This will print each specific docID
+
   let rowDiv = document.querySelector(".row");
+  
+  // Modify the buttons' IDs to include the docID so they're unique
   let tempDiv = `
-<div class="col">
-<div class="card h-100">
-<div class="card-body">
-<h5 class="card-title">${posts}</h5>
-<p class="card-text">Written By =>${ username}</p>
-<a href="#" class="card-link" id="Update">Update Post</a>
-<a href="#" class="card-link" id="delete">Delete Post</a>
-</div>
-</div>
-</div>
-`
-rowDiv.innerHTML += tempDiv;
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">${posts}</h5>
+        <p class="card-text">Written By =>${username}</p>
+        <a href="#" class="card-link" id="Update-${docID}">Update Post</a>
+        <a href="#" class="card-link" id="Delete-${docID}">Delete Post</a>
+      </div>
+    </div>
+  </div>
+  `;
 
-let deleteBttn = document.getElementById("delete");
-deleteBttn.addEventListener("click", async () => {
-  await deleteDoc(doc(db, "Posts", docID));
-  window.location.reload();
-})
+  rowDiv.innerHTML += tempDiv;
 
-let updateBttn = document.getElementById("Update");
-updateBttn.addEventListener("click", async () => {
-  updateFunc(docID)
-  let input = prompt(" enter here",posts)
-  console.log(input)
-})
+  // Get the unique buttons based on docID
+  let deleteBttn = document.getElementById(`Delete-${docID}`);
+  let updateBttn = document.getElementById(`Update-${docID}`);
+  // let updateBttn = document.getElementById(`Update`);
 
+  // Event listener for the delete button
+  deleteBttn.addEventListener("click", async () => {
+    await deleteDoc(doc(db, "Posts", docID));  // Delete the document by docID
+    window.location.reload();  // Reload to reflect changes
+  });
+
+  // Event listener for the update button
+  updateBttn.addEventListener("click", async () => {
+    updateFunc(docID);  // Call your update function here (if necessary)
+    let input = prompt("Enter here", posts);  // Prompt the user to enter a new post
+    if (input !== null) {
+      // Make sure the input is not null before updating the post
+      await updateDoc(doc(db, "Posts", docID), { post: input });
+      console.log(input);  // Log the new post content
+      window.location.reload();  // Reload to reflect the update
+    }
+  });
 });
-
